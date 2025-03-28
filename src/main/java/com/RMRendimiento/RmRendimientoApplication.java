07d19a55fbd1f6798b37454ec666fa3dd062c415
Emanuel Arispe <ema.arispe@gmail.com>
2025-03-28 19:33:31 -0300

    Initialized Proyect

---
+++ src/main/java/com/RMRendimiento/RmRendimientoApplication.java
@@ -0,0 +1,13 @@
+package com.RMRendimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmRendimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmRendimientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RMRendimiento/RmRendimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RMRendimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmRendimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
