3b1262021f0215639160a35af705d9dc2b810c2b
Emanuel <123778852+EmanuelArispe@users.noreply.github.com>
2025-03-29 17:44:56 -0300

    Feature/create gateway (#2)
    * dependencies are added for jwt
    
    * added cors configuration
    
    * Merging feature/createGateway
    
    * Added SecurityConfig witch PasswordEncoder
    
    * Update mave.yml
    
    * Update mave.yml [Delete dependency graph]

---
+++ src/main/java/com/RMRendimiento/Gateway/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.RMRendimiento.Gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("http://localhost:5173/") // Solamente se permite el acceso desde el puerto 5173 o URL de la aplicacion
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
