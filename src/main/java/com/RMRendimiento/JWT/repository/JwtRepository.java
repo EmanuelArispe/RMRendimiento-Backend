c7e9d91aa29e0a9257b0d6a72957e4a776e88118
FranciscoVazquez <franciscovazquez211@gmail.com>
2025-04-14 10:52:55 -0300

    Fix JWT Service and JwtRepository With UserRole lookUP

--- src/main/java/com/RMRendimiento/JWT/repository/JwtRepository.java
+++ src/main/java/com/RMRendimiento/JWT/repository/JwtRepository.java
@@ -8,8 +8,7 @@ import java.util.Optional;
 public interface JwtRepository extends JpaRepository<User,Long> {
 
     @Query("""
-        FROM User u JOIN FETCH u.roles
-        WHERE lower(u.name) = lower(?1)
+        FROM User u WHERE lower(u.name) = lower(?1)
     """)
     Optional<User> findByName(String name);
 }
--- src/main/java/com/RMRendimiento/JWT/service/JwtService.java
+++ src/main/java/com/RMRendimiento/JWT/service/JwtService.java
@@ -3,7 +3,12 @@ package com.RMRendimiento.JWT.service;
 import com.RMRendimiento.JWT.dto.RoleTokenDTO;
 import com.RMRendimiento.JWT.dto.UserTokenDTO;
 import com.RMRendimiento.JWT.repository.JwtRepository;
+import com.RMRendimiento.Role.Role;
+import com.RMRendimiento.Role.RoleRepository;
 import com.RMRendimiento.User.User;
+import com.RMRendimiento.UserRole.UserRole;
+import com.RMRendimiento.UserRole.UserRoleRepository;
+import com.RMRendimiento.UserRole.dto.UserRoleDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,20 +19,31 @@ import java.util.Optional;
 public class JwtService {
 
     private final JwtRepository jwtRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final RoleRepository roleRepository;
 
-    public JwtService(JwtRepository jwtRepository) {
+
+    public JwtService(JwtRepository jwtRepository, UserRoleRepository userRoleRepository, RoleRepository roleRepository) {
         this.jwtRepository = jwtRepository;
+        this.userRoleRepository = userRoleRepository;
+        this.roleRepository = roleRepository;
     }
 
     public Optional<UserTokenDTO> findOneUserByName(String name) {
 
         Optional<User> userOptional = jwtRepository.findByName(name);
-
-
         List<RoleTokenDTO> roles = new ArrayList<>();
+
         if (userOptional.isPresent()) {
             User user = userOptional.get();
-            user.getRoles().forEach(role -> roles.add(new RoleTokenDTO(role.getId(), role.getRole())));
+            List<UserRoleDTO> userRoleDTO = userRoleRepository.findeByUserId(user.getId());
+            if(userRoleDTO.isEmpty()) {
+                return Optional.empty();
+            }
+            for (UserRoleDTO ur : userRoleDTO ){
+                roleRepository.findById(ur.getId_role()).ifPresent(role->
+                        roles.add(new RoleTokenDTO(role.getId(),role.getRole())));
+            }
             return Optional.of (UserTokenDTO.builder().id(user.getId()).name(user.getName()).email(user.getEmail()).password(user.getPassword()).roles(roles).build());
         } else {
             // If the user is not found, return throw an exception
